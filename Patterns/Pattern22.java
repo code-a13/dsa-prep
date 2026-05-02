aab3c991c08adebe3cfad4067cf5f254865f9a54
code-a13 <adityakrishnamoorthi13@gmail.com>
2026-05-02 18:52:01 +0530

    Patterns

---
+++ Patterns/Pattern22.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Pattern22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+        pattern22(n);
+        sc.close();
+
+    }
+    public static void pattern22(int n) {
+        
+        for (int i = 0; i < 2 * n - 1; i++) {
+            
+            for (int j = 0; j < 2 * n - 1; j++) {
+                
+                int top = i;
+                int left = j;
+                int right = (2 * n - 2) - j;
+                int bottom = (2 * n - 2) - i;
+              
+                System.out.print((n - Math.min(Math.min(top, bottom), Math.min(left, right))) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
