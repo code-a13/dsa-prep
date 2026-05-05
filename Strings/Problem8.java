ba1631ddfc8d44f983b768efb11b140e7c6fee66
code-a13 <adityakrishnamoorthi13@gmail.com>
2026-05-05 16:50:48 +0530

    Basic Strings

---
+++ Strings/Problem8.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+public class Problem8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String: ");
+        String input = sc.nextLine();
+        List<Character> sortedChars = new Problem8().frequencySort(input);
+        for (char ch : sortedChars) {
+            System.out.print(ch);
+        }
+        sc.close();
+    }
+    public  List<Character> frequencySort(String s) {
+    
+    Pair[] freq = new Pair[26];
+    for (int i = 0; i < 26; i++) {
+        freq[i] = new Pair(0, (char)(i + 'a'));
+    }
+
+    for (char ch : s.toCharArray()) {
+        freq[ch - 'a'].freq++;
+    }
+
+    Arrays.sort(freq, (p1, p2) -> {
+        if (p1.freq != p2.freq) return p2.freq - p1.freq;
+        return p1.ch - p2.ch;
+    });
+
+
+    List<Character> result = new ArrayList<>();
+    for (Pair p : freq) {
+        if (p.freq > 0) result.add(p.ch);
+    }
+    return result;
+}
+
+class Pair {
+    int freq;
+    char ch;
+    Pair(int f, char c) {
+        this.freq = f;
+        this.ch = c;
+    }
+}
+}
