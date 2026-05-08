4ace4de8f4f9744af4b266254e7ca33756106008
code-a13 <adityakrishnamoorthi13@gmail.com>
2026-05-08 19:20:23 +0530

    Patterns

--- Patterns/Pattern21.java
+++ Patterns/Pattern21.java
@@ -2,16 +2,15 @@ import java.util.Scanner;
 public class Pattern21 {
     public void pattern21(int n) {
         
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                // Check if it is  boundary
-                if(i == 0 || i == n - 1 || j == 0 || j == n - 1) {
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=n;j++){
+                if(i == 1 || i == n || j == 1 || j == n){
                     System.out.print("*");
-                } else {
+                }
+                else{
                     System.out.print(" ");
                 }
             }
-            
             System.out.println();
         }
     }
