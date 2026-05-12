4853abc7013a090945f097d993b82d04a6afb2e4
code-a13 <adityakrishnamoorthi13@gmail.com>
2026-05-12 18:32:15 +0530

    Pattern  Practice

--- Patterns/Pattern23.java
+++ Patterns/Pattern23.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Pattern23 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
         int n = sc.nextInt();
         pattern23(n);
         sc.close();
