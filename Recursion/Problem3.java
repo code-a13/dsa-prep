e684857a3cf57c30d38993a73db9f012c45b9521
code-a13 <adityakrishnamoorthi13@gmail.com>
2026-05-17 17:47:14 +0530

    Recursion

---
+++ Recursion/Problem3.java
@@ -0,0 +1,27 @@
+package Recursion;
+import java.util.Scanner;
+public class Problem3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int size = scanner.nextInt();
+        int[] nums = new int[size];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        Problem3 p = new Problem3();
+        System.out.println("The sum of the array elements is: " + p.arraySum(nums));
+    }
+    public int arraySum(int[] nums) {
+        return sum(nums, 0);
+    }
+
+    private int sum(int[] nums, int left) {
+        if (left >= nums.length) {
+            return 0;
+        }
+        
+        return nums[left] + sum(nums, left + 1);
+    }
+}
