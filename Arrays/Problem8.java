ac4b9876790951c938b5f2f9a2cf7d418bc47c56
code-a13 <adityakrishnamoorthi13@gmail.com>
2026-07-12 20:54:49 +0530

    Array Problem

---
+++ Arrays/Problem8.java
@@ -0,0 +1,37 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class Problem8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int n = sc.nextInt();
+        int nums[] = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for(int i = 0;i<n;i++){
+            nums[i] = sc.nextInt();
+        }
+        int result = findMaxConsecutiveOnes(nums);
+        System.out.println("Maximum consecutive 1's in the array is: " + result);
+        sc.close();
+    }
+    public static int findMaxConsecutiveOnes(int[] nums) {  
+        int max = 0;
+        int count = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i]==1){
+                max++;
+            }
+            else{
+                max = 0;
+            }
+
+            if(count<max){
+                count = max;
+            }
+            
+        }
+        return count;
+    }
+}
